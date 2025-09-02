c1334859eba2d72723feccce6cfe74b87f98dfae
ssl3p <professionnelmoro@gmail.com>
2025-09-02 14:50:11 +0200

    Update cl_interface.lua

--- sleep_death_screen/lua/ssl3p/client/cl_interface.lua
+++ sleep_death_screen/lua/ssl3p/client/cl_interface.lua
@@ -1,3 +1,53 @@
+ssl3pLib = ssl3pLib or {}
+
+if not ssl3pLib.RespX then
+    local BASE_W, BASE_H = 1920, 1080
+
+    function ssl3pLib.RespX(x)
+        return math.floor((ScrW() / BASE_W) * (tonumber(x) or 0))
+    end
+
+    function ssl3pLib.RespY(y)
+        return math.floor((ScrH() / BASE_H) * (tonumber(y) or 0))
+    end
+
+    local _fontCache, _weightMap = {}, { Light = 300, Regular = 400, Medium = 500, Bold = 700, Black = 900 }
+
+    local function fontKey(size, weight)
+        return string.format("ssl3pCompat_%d_%s", size, tostring(weight or "Regular"))
+    end
+
+    function ssl3pLib:Font(size, weightName)
+        size = tonumber(size) or 16
+        weightName = weightName or "Regular"
+        local key = fontKey(size, weightName)
+        if not _fontCache[key] then
+            surface.CreateFont(key, {
+                font = "Montserrat",
+                size = size,
+                weight = _weightMap[weightName] or 400,
+                antialias = true,
+                extended = true,
+            })
+            _fontCache[key] = true
+        end
+        return key
+    end
+
+    function ssl3pLib.DrawMaterial(x, y, w, h, matOrPath, col)
+        if not matOrPath then return end
+        local mat = isstring(matOrPath) and Material(matOrPath, "smooth") or matOrPath
+        surface.SetDrawColor(col or color_white)
+        surface.SetMaterial(mat)
+        surface.DrawTexturedRect(x, y, w, h)
+    end
+
+    function ssl3pLib:Sound(name)
+        local map = { Hover = "common/NULL.wav", Click = "ssl3p/click.wav" }
+        if map[name] then surface.PlaySound(map[name]) end
+    end
+end
+
 local PRX, PRY = ssl3pLib.RespX, ssl3pLib.RespY
 local CFG = ssl3pDeath and ssl3pDeath.Config or {}
 
