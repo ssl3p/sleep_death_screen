b48f5e5aecb2fcf3ca7020a3484f358ccf313de4
ssl3p <professionnelmoro@gmail.com>
2025-10-05 11:38:28 +0200

    Update config.lua

--- sleep_death_screen/lua/ssl3p/config.lua
+++ sleep_death_screen/lua/ssl3p/config.lua
@@ -7,6 +7,6 @@ SSL3P.Config.Title = "Tu es"
 SSL3P.Config.Highlight = "Inconscient"
 SSL3P.Config.Subtitle = "Vous êtes mort vous allez bientôt réaparaitre. Si vous pensez que c'est un freekill faites un ticket."
 
-SSL3P.Config.SkullPath = "sleep_librairies/skull.png" -- Image death screen
+SSL3P.Config.SkullPath = "ssl3p/skull.png" -- Image death screen
 SSL3P.Config.TicketCommand = "!report" -- Commande pour le bouton ticket
 SSL3P.Config.EnableBlur = true -- Effet de flou
